37bf6edfe9718eb9646f2203e8f7a7a7ad01cbf0
PauloCampana <paulo.campana@academico.ufpb.br>
2023-10-23 00:57:14 -0300

    type generic generators

--- src/RandomVariable.zig
+++ src/RandomVariable.zig
@@ -1,155 +1,153 @@
-//! Generators for Random Variables with different distributions.
-
 const std = @import("std");
-const math = std.math;
-const RandomVariable = @This();
 
-// fields in struct
+pub fn RandomVariable(comptime Tu: type, comptime Tf: type) type {
+    return struct {
+        const Self = @This();
+        generator: std.rand.Random,
 
-generator: std.rand.Random,
-comptime T: type = f64,
+        pub fn init(r: std.rand.Random) Self {
+            return Self {.generator = r};
+        }
 
-// functions you can call with rv.fun()
+        pub fn uniform(self: Self, min: Tf, max: Tf) Tf {
+            const uni = self.generator.float(Tf);
+            return min + (max - min) * uni;
+        }
 
-pub fn uniform(self: *RandomVariable, min: f64, max: f64) f64 {
-    const uni = self.generator.float(self.T);
-    return min + (max - min) * uni;
-}
+        pub fn bernoulli(self: Self, prob: Tf) bool {
+            const uni = self.generator.float(Tf);
+            const ber = uni < prob;
+            return ber;
+        }
 
-pub fn bernoulli(self: *RandomVariable, prob: f64) bool {
-    const uni = self.generator.float(self.T);
-    const ber = uni < prob;
-    return ber;
-}
+        pub fn geometric(self: Self, prob: Tf) Tu {
+            const uni = self.generator.float(Tf);
+            const geo = @floor(@log(uni) / std.math.log1p(-prob));
+            return @intFromFloat(geo);
+        }
 
-pub fn geometric(self: *RandomVariable, prob: f64) u64 {
-    const uni = self.generator.float(self.T);
-    const geo = @floor(@log(uni) / math.log1p(-prob));
-    return @intFromFloat(geo);
-}
+        pub fn poisson(self: Self, lambda: Tf) Tu {
+            const uni = self.generator.float(Tf);
+            var p = @exp(-lambda);
+            var f = p;
+            var poi: Tu = 0;
+            while (uni >= f) : (poi += 1) {
+                p *= lambda / @as(Tf, @floatFromInt(poi + 1));
+                f += p;
+            }
+            return poi;
+        }
 
-pub fn poisson(self: *RandomVariable, lambda: f64) u64 {
-    const uni = self.generator.float(self.T);
-    var p = @exp(-lambda);
-    var f = p;
-    var poi: u64 = 0;
-    while (uni >= f) : (poi += 1) {
-        p *= lambda / @as(f64, poi + 1);
-        f += p;
-    }
-    return poi;
-}
+        pub fn binomial(self: Self, size: Tu, prob: Tf) Tu {
+            var bin: Tu = 0;
+            for (0..size) |_| {
+                bin += @intFromBool(bernoulli(self, prob));
+            }
+            return bin;
+        }
 
-pub fn binomial(self: *RandomVariable, size: u64, prob: f64) u64 {
-    var bin: u64 = 0;
-    for (0..size) |_| {
-        bin += @as(u64, bernoulli(self, prob));
-    }
-    return bin;
-}
+        pub fn negativeBinomial(self: Self, size: Tu, prob: Tf) Tu {
+            var nbin: Tu = 0;
+            for (0..size) |_| {
+                nbin += geometric(self, prob);
+            }
+            return nbin;
+        }
 
-pub fn negative_binomial(self: *RandomVariable, size: u64, prob: f64) u64 {
-    var nbin: u64 = 0;
-    for (0..size) |_| {
-        nbin += geometric(self, prob);
-    }
-    return nbin;
-}
+        pub fn exponential(self: Self, rate: Tf) Tf {
+            const exp = self.generator.floatExp(Tf) / rate;
+            return exp;
+        }
 
-pub fn exponential(self: *RandomVariable, rate: f64) f64 {
-    const exp = self.generator.floatExp(f64) / rate;
-    return exp;
-}
+        pub fn weibull(self: Self, shape: Tf, rate: Tf) Tf {
+            const uni = self.generator.float(Tf);
+            const wei = std.math.pow(Tf, -@log(uni), 1 / shape) / rate;
+            return wei;
+        }
 
-pub fn weibull(self: *RandomVariable, shape: f64, rate: f64) f64 {
-    const uni = self.generator.float(self.T);
-    const wei = math.pow(f64, -@log(uni), 1 / shape) / rate;
-    return wei;
-}
+        pub fn cauchy(self: Self, location: Tf, scale: Tf) Tf {
+            const uni = self.generator.float(Tf);
+            const cau = location + scale * @tan(std.math.pi * uni);
+            return cau;
+        }
 
-pub fn cauchy(self: *RandomVariable, location: f64, scale: f64) f64 {
-    const uni = self.generator.float(self.T);
-    const cau = location + scale * @tan(math.pi * uni);
-    return cau;
-}
+        pub fn logistic(self: Self, location: Tf, scale: Tf) Tf {
+            const uni = self.generator.float(Tf);
+            const log = location + scale * @log(uni / (1 - uni));
+            return log;
+        }
 
-pub fn logistic(self: *RandomVariable, location: f64, scale: f64) f64 {
-    const uni = self.generator.float(self.T);
-    const log = location + scale * @log(uni / (1 - uni));
-    return log;
-}
+        pub fn gamma(self: Self, shape: Tf, rate: Tf) Tf {
+            const correct = shape >= 1;
+            const d = blk: {
+                const d = shape - 1.0 / 3.0;
+                break: blk if (shape >= 1) d else d + 1;
+            };
+            const c = 1 / @sqrt(9 * d);
 
-pub fn gamma(self: *RandomVariable, shape: f64, rate: f64) f64 {
-    const correct = shape >= 1;
-    const d1 = shape - 1.0 / 3.0;
-    const d = if (correct) d1 else d1 + 1;
-    const c = 1 / @sqrt(9 * d);
+            var v: Tf = undefined;
+            while (true) {
+                var z: Tf = undefined;
+                while (true) {
+                    z = normal(self, 0, 1);
+                    v = 1 + c * z;
+                    if (v > 0) {
+                        break;
+                    }
+                }
+                v *= v * v;
+                z *= z;
+                const uni = self.generator.float(Tf);
+                if (
+                    uni < 1 - 0.0331 * z * z or
+                    @log(uni) < 0.5 * z + d * (1 - v + @log(v))
+                ) {
+                    break;
+                }
+            }
+            const gam = d * v;
 
-    var v: f64 = undefined;
-    while (true) {
-        var z: f64 = undefined;
-        while (true) {
-            z = normal(self, 0, 1);
-            v = 1 + c * z;
-            if (v > 0) {
-                break;
+            if (correct) {
+                return gam / rate;
+            } else {
+                const uni = self.generator.float(Tf);
+                const correction = std.math.pow(Tf, uni, 1 / shape);
+                return gam * correction / rate;
             }
         }
-        v *= v * v;
-        z *= z;
-        const uni = self.generator.float(self.T);
-        if (
-            uni < 1 - 0.0331 * z * z or
-            @log(uni) < 0.5 * z + d * (1 - v + @log(v))
-        ) {
-            break;
+
+        pub fn chiSquared(self: Self, df: Tf) Tf {
+            return gamma(self, 0.5 * df, 0.5);
         }
-    }
-    const gam = d * v;
 
-    if (correct) {
-        return gam / rate;
-    } else {
-        const uni = self.generator.float(self.T);
-        const correction = math.pow(f64, uni, 1 / shape);
-        return gam * correction / rate;
-    }
+        pub fn F(self: Self, df1: Tf, df2: Tf) Tf {
+            const chi1 = chiSquared(self, df1);
+            const chi2 = chiSquared(self, df2);
+            const f = chi1 / chi2 / df1 * df2;
+            return f;
+        }
+
+        pub fn beta(self: Self, shape1: Tf, shape2: Tf) Tf {
+            const gam1 = gamma(self, shape1, 1);
+            const gam2 = gamma(self, shape2, 1);
+            const bet = gam1 / (gam1 + gam2);
+            return bet;
+        }
+
+        pub fn normal(self: Self, mean: Tf, sd: Tf) Tf {
+            const nor = self.generator.floatNorm(Tf);
+            return mean + sd * nor;
+        }
+
+        pub fn logNormal(self: Self, meanlog: Tf, sdlog: Tf) Tf {
+            return @exp(normal(self, meanlog, sdlog));
+        }
+
+        pub fn t(self: Self, df: Tf) Tf {
+            const nor = normal(self, 0, 1);
+            const chi = chiSquared(self, df);
+            const T = nor * @sqrt(df / chi);
+            return T;
+        }
+    };
 }
-
-pub fn chi_squared(self: *RandomVariable, df: f64) f64 {
-    return gamma(self, 0.5 * df, 0.5);
-}
-
-pub fn F(self: *RandomVariable, df1: f64, df2: f64) f64 {
-    const chi1 = chi_squared(self, df1);
-    const chi2 = chi_squared(self, df2);
-    const f = chi1 / chi2 / df1 * df2;
-    return f;
-}
-
-pub fn beta(self: *RandomVariable, shape1: f64, shape2: f64) f64 {
-    const gam1 = gamma(self, shape1, 1);
-    const gam2 = gamma(self, shape2, 1);
-    const bet = gam1 / (gam1 + gam2);
-    return bet;
-}
-
-pub fn normal(self: *RandomVariable, mean: f64, sd: f64) f64 {
-    const nor = self.generator.floatNorm(f64);
-    return mean + sd * nor;
-}
-
-pub fn lognormal(self: *RandomVariable, meanlog: f64, sdlog: f64) f64 {
-    return @exp(normal(self, meanlog, sdlog));
-}
-
-pub fn t(self: *RandomVariable, df: f64) f64 {
-    const nor = normal(self, 0, 1);
-    const chi = chi_squared(self, df);
-    const T = nor * @sqrt(df / chi);
-    return T;
-}
-
-// TODO: remove f64s, use T
-// TODO: bounds check for parameters
-// TODO: special cases of the distros
--- src/main.zig
+++ src/main.zig
@@ -2,15 +2,95 @@ const std = @import("std");
 const RandomVariable = @import("RandomVariable.zig");
 
 pub fn main() !void {
-    const nano: u64 = @intCast(std.time.nanoTimestamp());
-    var gen = std.rand.DefaultPrng.init(nano);
-    var rv = RandomVariable {.generator = gen.random()};
+    var gen = std.rand.DefaultPrng.init(@intCast(std.time.nanoTimestamp()));
+    var rv = RandomVariable.RandomVariable(u23, f64).init(gen.random());
 
+    std.debug.print("{s}\n", .{@typeName(@TypeOf(rv.binomial(10,0.5)))});
+
+    std.debug.print("\n{s:17}", .{"uniform:"});
     for (0..10) |_| {
-        for (0..10) |_| {
-            const x = rv.gamma(3,5);
-            std.debug.print("{d:10.3}", .{x});
-        }
-        std.debug.print("\n", .{});
+        const x = rv.uniform(0, 1);
+        std.debug.print("{d:10.3}", .{x});
     }
+    std.debug.print("\n{s:17}", .{"bernoulli:"});
+    for (0..10) |_| {
+        const x = rv.bernoulli(0.5);
+        std.debug.print("{:10.3}", .{x});
+    }
+    std.debug.print("\n{s:17}", .{"geometric:"});
+    for (0..10) |_| {
+        const x = rv.geometric(0.5);
+        std.debug.print("{d:10.3}", .{x});
+    }
+    std.debug.print("\n{s:17}", .{"poisson:"});
+    for (0..10) |_| {
+        const x = rv.poisson(5);
+        std.debug.print("{d:10.3}", .{x});
+    }
+    std.debug.print("\n{s:17}", .{"binomial:"});
+    for (0..10) |_| {
+        const x = rv.binomial(5, 0.5);
+        std.debug.print("{d:10.3}", .{x});
+    }
+    std.debug.print("\n{s:17}", .{"negativeBinomial:"});
+    for (0..10) |_| {
+        const x = rv.negativeBinomial(5, 0.5);
+        std.debug.print("{d:10.3}", .{x});
+    }
+    std.debug.print("\n{s:17}", .{"exponential:"});
+    for (0..10) |_| {
+        const x = rv.exponential(5);
+        std.debug.print("{d:10.3}", .{x});
+    }
+    std.debug.print("\n{s:17}", .{"weibull:"});
+    for (0..10) |_| {
+        const x = rv.weibull(5, 5);
+        std.debug.print("{d:10.3}", .{x});
+    }
+    std.debug.print("\n{s:17}", .{"cauchy:"});
+    for (0..10) |_| {
+        const x = rv.cauchy(0, 1);
+        std.debug.print("{d:10.3}", .{x});
+    }
+    std.debug.print("\n{s:17}", .{"logistic:"});
+    for (0..10) |_| {
+        const x = rv.logistic(0, 1);
+        std.debug.print("{d:10.3}", .{x});
+    }
+    std.debug.print("\n{s:17}", .{"gamma:"});
+    for (0..10) |_| {
+        const x = rv.gamma(5, 5);
+        std.debug.print("{d:10.3}", .{x});
+    }
+    std.debug.print("\n{s:17}", .{"chiSquared:"});
+    for (0..10) |_| {
+        const x = rv.chiSquared(5);
+        std.debug.print("{d:10.3}", .{x});
+    }
+    std.debug.print("\n{s:17}", .{"F:"});
+    for (0..10) |_| {
+        const x = rv.F(5, 5);
+        std.debug.print("{d:10.3}", .{x});
+    }
+    std.debug.print("\n{s:17}", .{"beta:"});
+    for (0..10) |_| {
+        const x = rv.beta(5, 5);
+        std.debug.print("{d:10.3}", .{x});
+    }
+    std.debug.print("\n{s:17}", .{"normal:"});
+    for (0..10) |_| {
+        const x = rv.normal(0, 1);
+        std.debug.print("{d:10.3}", .{x});
+    }
+    std.debug.print("\n{s:17}", .{"logNormal:"});
+    for (0..10) |_| {
+        const x = rv.logNormal(0, 1);
+        std.debug.print("{d:10.3}", .{x});
+    }
+    std.debug.print("\n{s:17}", .{"t:"});
+    for (0..10) |_| {
+        const x = rv.t(5);
+        std.debug.print("{d:10.3}", .{x});
+    }
+    std.debug.print("\n", .{});
 }
