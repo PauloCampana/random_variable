0dea2f8951e1a239ee2699d2b1c2de1a668b23b4
PauloCampana <paulo.campana@academico.ufpb.br>
2023-10-30 22:06:06 -0300

    fill slice with random

--- src/main.zig
+++ src/main.zig
@@ -2,95 +2,32 @@ const std = @import("std");
 const RandomVariable = @import("RandomVariable.zig");
 
 pub fn main() !void {
-    var gen = std.rand.DefaultPrng.init(@intCast(std.time.nanoTimestamp()));
-    var rv = RandomVariable.RandomVariable(u23, f64).init(gen.random());
+    var gen = std.rand.DefaultPrng
+        .init(@intCast(std.time.nanoTimestamp()));
+    var gpa = std.heap.GeneralPurposeAllocator(.{}) {};
+    var rv = RandomVariable
+        .setType(u64, f64)
+        .init(gen.random(), gpa.allocator());
 
-    std.debug.print("{s}\n", .{@typeName(@TypeOf(rv.binomial(10,0.5)))});
+    try testrv(rv);
+}
 
-    std.debug.print("\n{s:17}", .{"uniform:"});
-    for (0..10) |_| {
-        const x = rv.uniform(0, 1);
-        std.debug.print("{d:10.3}", .{x});
-    }
-    std.debug.print("\n{s:17}", .{"bernoulli:"});
-    for (0..10) |_| {
-        const x = rv.bernoulli(0.5);
-        std.debug.print("{:10.3}", .{x});
-    }
-    std.debug.print("\n{s:17}", .{"geometric:"});
-    for (0..10) |_| {
-        const x = rv.geometric(0.5);
-        std.debug.print("{d:10.3}", .{x});
-    }
-    std.debug.print("\n{s:17}", .{"poisson:"});
-    for (0..10) |_| {
-        const x = rv.poisson(5);
-        std.debug.print("{d:10.3}", .{x});
-    }
-    std.debug.print("\n{s:17}", .{"binomial:"});
-    for (0..10) |_| {
-        const x = rv.binomial(5, 0.5);
-        std.debug.print("{d:10.3}", .{x});
-    }
-    std.debug.print("\n{s:17}", .{"negativeBinomial:"});
-    for (0..10) |_| {
-        const x = rv.negativeBinomial(5, 0.5);
-        std.debug.print("{d:10.3}", .{x});
-    }
-    std.debug.print("\n{s:17}", .{"exponential:"});
-    for (0..10) |_| {
-        const x = rv.exponential(5);
-        std.debug.print("{d:10.3}", .{x});
-    }
-    std.debug.print("\n{s:17}", .{"weibull:"});
-    for (0..10) |_| {
-        const x = rv.weibull(5, 5);
-        std.debug.print("{d:10.3}", .{x});
-    }
-    std.debug.print("\n{s:17}", .{"cauchy:"});
-    for (0..10) |_| {
-        const x = rv.cauchy(0, 1);
-        std.debug.print("{d:10.3}", .{x});
-    }
-    std.debug.print("\n{s:17}", .{"logistic:"});
-    for (0..10) |_| {
-        const x = rv.logistic(0, 1);
-        std.debug.print("{d:10.3}", .{x});
-    }
-    std.debug.print("\n{s:17}", .{"gamma:"});
-    for (0..10) |_| {
-        const x = rv.gamma(5, 5);
-        std.debug.print("{d:10.3}", .{x});
-    }
-    std.debug.print("\n{s:17}", .{"chiSquared:"});
-    for (0..10) |_| {
-        const x = rv.chiSquared(5);
-        std.debug.print("{d:10.3}", .{x});
-    }
-    std.debug.print("\n{s:17}", .{"F:"});
-    for (0..10) |_| {
-        const x = rv.F(5, 5);
-        std.debug.print("{d:10.3}", .{x});
-    }
-    std.debug.print("\n{s:17}", .{"beta:"});
-    for (0..10) |_| {
-        const x = rv.beta(5, 5);
-        std.debug.print("{d:10.3}", .{x});
-    }
-    std.debug.print("\n{s:17}", .{"normal:"});
-    for (0..10) |_| {
-        const x = rv.normal(0, 1);
-        std.debug.print("{d:10.3}", .{x});
-    }
-    std.debug.print("\n{s:17}", .{"logNormal:"});
-    for (0..10) |_| {
-        const x = rv.logNormal(0, 1);
-        std.debug.print("{d:10.3}", .{x});
-    }
-    std.debug.print("\n{s:17}", .{"t:"});
-    for (0..10) |_| {
-        const x = rv.t(5);
-        std.debug.print("{d:10.3}", .{x});
-    }
-    std.debug.print("\n", .{});
+pub fn testrv(rv: anytype) !void {
+    std.debug.print("uni {d:10.3}\n", .{try rv.uniformSlice(10, 0, 1)});
+    std.debug.print("ber {any:10}\n", .{try rv.bernoulliSlice(10, 0.5)});
+    std.debug.print("geo {d:10.3}\n", .{try rv.geometricSlice(10, 0.5)});
+    std.debug.print("poi {d:10.3}\n", .{try rv.poissonSlice(10, 5)});
+    std.debug.print("bin {d:10.3}\n", .{try rv.binomialSlice(10, 10, 0.5)});
+    std.debug.print("nbi {d:10.3}\n", .{try rv.negativeBinomialSlice(10, 10, 0.5)});
+    std.debug.print("exp {d:10.3}\n", .{try rv.exponentialSlice(10, 0.5)});
+    std.debug.print("wei {d:10.3}\n", .{try rv.weibullSlice(10, 5, 0.5)});
+    std.debug.print("cau {d:10.3}\n", .{try rv.cauchySlice(10, 0, 1)});
+    std.debug.print("log {d:10.3}\n", .{try rv.logisticSlice(10, 0, 1)});
+    std.debug.print("gam {d:10.3}\n", .{try rv.gammaSlice(10, 5, 5)});
+    std.debug.print("chi {d:10.3}\n", .{try rv.chiSquaredSlice(10, 5)});
+    std.debug.print("F   {d:10.3}\n", .{try rv.FSlice(10, 5, 5)});
+    std.debug.print("bet {d:10.3}\n", .{try rv.betaSlice(10, 5, 5)});
+    std.debug.print("nor {d:10.3}\n", .{try rv.normalSlice(10, 0, 1)});
+    std.debug.print("lno {d:10.3}\n", .{try rv.logNormalSlice(10, 0, 1)});
+    std.debug.print("t   {d:10.3}\n", .{try rv.tSlice(10, 5)});
 }
