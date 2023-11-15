c2283a45157c481cdba3c64c4463e34b25cf2728
PauloCampana <paulo.campana@academico.ufpb.br>
2023-11-15 19:07:10 -0300

    fancy docs

--- build.zig
+++ build.zig
@@ -17,8 +17,17 @@ pub fn build(b: *std.Build) !void {
         .target = target,
         .optimize = optimize,
     });
+
     const test_cmd = b.addRunArtifact(tests);
     test_cmd.step.dependOn(b.getInstallStep());
-    const test_step = b.step("test", "Run library tests");
+    const test_step = b.step("test", "Run tests");
     test_step.dependOn(&test_cmd.step);
+
+    const docs_cmd = b.addInstallDirectory(.{
+        .source_dir = tests.getEmittedDocs(),
+        .install_dir = std.Build.InstallDir {.custom = ".."},
+        .install_subdir = "docs",
+    });
+    const docs_step = b.step("docs", "Generate documentation");
+    docs_step.dependOn(&docs_cmd.step);
 }
