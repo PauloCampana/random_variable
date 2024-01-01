a1e9be4d4fd22d2d2aae9d253604140d61d19a5a
PauloCampana <paulo.campana@academico.ufpb.br>
2023-12-31 22:29:01 -0300

    quantile interpolation, more tests

--- docs/data-modules.js
+++ docs/data-modules.js
@@ -1,1 +1,1 @@
-var modules =[{"name":"root","file":0,"main":66,"table":{"root":0,"std":1}},{"name":"std","file":0,"main":69,"table":{}}];
+var modules =[{"name":"root","file":0,"main":66,"table":{"std":1,"root":0}},{"name":"std","file":0,"main":69,"table":{}}];
