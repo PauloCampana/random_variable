070db92de124b64a4a3202aa297b7e0c4bdad4ce
PauloCampana <paulo.campana@academico.ufpb.br>
2024-06-23 20:15:43 -0300

    update to zig 0.13, simpler readme

--- README.md
+++ README.md
@@ -9,33 +9,23 @@ For over 30 probability distributions, this library provides:
 Avaliable on <https://paulocampana.github.io/random_variable>
 
 # Importing library
-Requires Zig version 0.12.0 or above.
-
-Grab the commit hash of the version you want to use and in your `build.zig.zon`, add inside the dependencies:
-
-```zig
-.random_variable = .{
-    .url = "https://github.com/paulocampana/random_variable/archive/v1.0.0.tar.gz",
-    // .hash = "leave this commented, compiler will tell you what to put here",
-},
+```bash
+$ zig fetch --save git+https://github.com/PauloCampana/random_variable
 ```
 
-You could also copy the whole repository somewhere and use the `.path` field instead of `.url`
-
-Then, on your `build.zig`, add the `dependency` at the top and the `addImport` for every exe/lib/tests you need:
-
 ```zig
-// ...
-const random_variable = b.dependency("random_variable", .{
+// build.zig
+const rv = b.dependency("random_variable", .{
     .target = target,
     .optimize = optimize,
 });
-// ...
-exe.root_module.addImport("random_variable", random_variable.module("random_variable"));
-```
 
-You can then import the library where you need
+exe.root_module.addImport("random_variable", rv.module("random_variable"));
+```
 
 ```zig
+// main.zig
 const rv = @import("random_variable");
 ```
+
+Or just copy the whole project into yours and import `src/root.zig`
--- build.zig
+++ build.zig
@@ -5,40 +5,40 @@ pub fn build(b: *std.Build) !void {
     const optimize = b.standardOptimizeOption(.{});
 
     const module = b.addModule("random_variable", .{
-        .root_source_file = .{ .path = "src/root.zig" },
+        .root_source_file = b.path("src/root.zig"),
     });
 
     const tests = b.addTest(.{
-        .root_source_file = .{ .path = "src/root.zig" },
+        .root_source_file = b.path("src/root.zig"),
         .target = target,
         .optimize = optimize,
     });
-    const test_cmd = b.addRunArtifact(tests);
+    const test_run = b.addRunArtifact(tests);
     const test_step = b.step("test", "Run library tests");
-    test_step.dependOn(&test_cmd.step);
+    test_step.dependOn(&test_run.step);
 
     const gof = b.addTest(.{
         .name = "gof",
-        .root_source_file = .{ .path = "src/gof_tests.zig" },
+        .root_source_file = b.path("src/gof_tests.zig"),
         .target = target,
         .optimize = optimize,
     });
     gof.root_module.addImport("random_variable", module);
-    const gof_cmd = b.addRunArtifact(gof);
+    const gof_run = b.addRunArtifact(gof);
     const gof_step = b.step("gof", "Run \"Goodness of fit\" tests for random variable generation");
-    gof_step.dependOn(&gof_cmd.step);
+    gof_step.dependOn(&gof_run.step);
 
     const docs = b.addObject(.{
         .name = "docs",
         .target = target,
         .optimize = optimize,
-        .root_source_file = .{ .path = "src/root.zig" },
+        .root_source_file = b.path("src/root.zig"),
     });
-    const docs_cmd = b.addInstallDirectory(.{
+    const docs_run = b.addInstallDirectory(.{
         .source_dir = docs.getEmittedDocs(),
         .install_dir = .prefix,
         .install_subdir = "../docs",
     });
     const docs_step = b.step("docs", "Generate documentation");
-    docs_step.dependOn(&docs_cmd.step);
+    docs_step.dependOn(&docs_run.step);
 }
